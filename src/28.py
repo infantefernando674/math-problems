f6ad9dd1d6cafcd15f69437984e5a213122d8a56
infantefernando674 <infantefernando674@gmail.com>
2025-04-08 09:23:33 +0000

    Add 28.py

---
+++ src/28.py
@@ -0,0 +1,6 @@
+# Example Python code snippet related to mathematical problems for educational purposes
+def square_root(x):
+    """ Calculates the square root of a number """
+    return x ** 0.5
+
+print(square_root(16))  # Output: 4
